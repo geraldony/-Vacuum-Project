2c824186735a8cda22a758013535ebbf5b189550
geraldony <onyedikageraldjnr@gmail.com>
2025-05-14 22:57:55 -0400

    Add files via upload
    This is the code that handles the driving and movement logic for the autononomous vaccum

---
+++ motorcode.h
@@ -0,0 +1,18 @@
+/*Author: Gerald Onyedika  */
+ /*Project: Autonomous Vaccum Robot */
+
+ //Include guard 
+ #ifndef MOTORCODE_H
+ #define MOTORCODE_H
+
+ // Declared Methods 
+ void setUpMotors();
+ void stopMotor();
+ void brakeMotor();
+ void driveFwd();
+ void driveBwd();
+void turnRight();
+void turnLeft();
+void uTurn();
+
+#endif
